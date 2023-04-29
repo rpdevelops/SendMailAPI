d6ad9fbf5dd0f45581c314cab0e1f064db93d8f5
Robson Paradella <paradellarobson@gmail.com>
2023-04-29 18:48:04 -0300

    Api Configured properly, receiving the post request and send a email with the content received

--- MailApi.mjs
+++ MailApi.mjs
@@ -3,8 +3,28 @@
 ------------------------MADE BY ROBSON PARADELLA ROCHA-----------------------
 ---------------------------------------------------------------------------*/
 import send_mail from "./MailSender.mjs";
+import express  from 'express';
+import bodyParser from "body-parser";
+//DEFINING A STRING OF SERVER
+const api = express();
+//USING A BODY PARSER TO GET THE VARIABLES SENT IN THE REQUEST
+api.use(bodyParser.json());
 
+//DEFINING A API ROUTE TO RECEIVE THE REQUESTS
+api.post('/mailapi/send-email', (req, res) => {
+  const subject = req.body.subject;
+  const toEmail = req.body.toEmail;
+  const body = req.body.body;
+//SEND EMAIL WITH THE VARIABLES SENT IN REQUEST.
+try {
+    send_mail(toEmail,subject ,body);
+    res.send('Email sent successfully');
+} catch (error) {
+    res.send(error);
+}
+});
 
-
-//SEND EMAIL FUNCTION USING
-send_mail('teste@gmail.com','segundoteste','testandoenviodeemail');
+// SERVER START
+api.listen(4001, () => {
+  console.log('Server Started, Route to API is :4001/mailapi/send-email');
+});
